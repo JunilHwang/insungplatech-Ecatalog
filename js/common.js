fc9e7c2f33510dc1da9c9a80293819117f962b6f
junil-hwang <tjsdlf4261@naver.com>
2018-02-04 15:12:04 +0900

    인성플라텍 전자카탈로그 완성본

---
+++ js/common.js
@@ -0,0 +1,141 @@
+//Variable Setting
+var am = new autoAnimation();
+
+//Document Event;
+$(pageLoadEvent)
+.on("click",".sub_view",subPageChange)
+.on("click",".main_view",mainView)
+.on('click','.sub-default .left, .sub-default .right',changePage)
+.on('click','.layerOpener',layerOpen)
+.on('click','.layer .close, .layer .bg',layerClose)
+$(window).on("keyup",function(event){
+	if($(".layer.active").length && event.keyCode == 27){
+		layerClose();
+	}
+})
+
+function layerClose(){
+	$(".layer").stop().fadeOut(300,function(){
+		$(this).removeClass("active")
+	})
+}
+
+//pageLoadEvent
+function pageLoadEvent(){
+
+	//Product Slide
+
+	//animation Class Setting and play
+	am.classSetting().change({obj:$('.main')}).play();
+	// am.classSetting().change({obj:$('.sub')}).play();
+	subView(".sub06");
+
+	if(opener) opener.close();
+}
+
+//subPageChange
+function subPageChange(){
+	subView($(this).data('menu'));
+}
+
+//mainView
+function mainView(){
+	if($('.main').hasClass('active')) return false;
+	am.callbackfn = function(){
+		callbackAnimation({type:'main'});
+	}
+	am.change({obj:$('.sub>nav, .sub>.active, .sub_footer'),reverse:'reverse'}).play();
+}
+
+//subView
+function subView(menu){
+	var callback, before;
+	if($('.sub').hasClass('active')){
+		before = $(".sub>.active");
+		callback = function(){
+			callbackAnimation({type:'subPart',menu:menu});
+		};
+	} else {
+		before = $('.main');
+		callback = function(){
+			callbackAnimation({type:'sub',menu:menu});
+		};
+	}
+	if(typeof menu == "string" && !$('.gnb '+menu).hasClass('active')){
+		$('.gnb .active').removeClass('active');
+		$('.gnb '+menu).addClass('active');
+	}
+	am.callbackfn = callback;
+	am.change({obj:before,reverse:'reverse'}).play();
+}
+
+//callback
+function callbackAnimation(option){
+	var before,after,afterPart;
+	switch(option.type){
+		case 'main' :
+			before = $('.sub');
+			after  = $('.main');
+		break;
+		case 'sub' :
+			before = $('.main');
+			after  = $('.sub');
+		break;
+		case 'subPart' :
+			before = $('.sub>.active');                        
+			after  = $(option.menu);
+		break;
+	}
+	before.fadeOut(300,function(){
+		before.removeClass('active');
+		$(this).removeAttr('style');
+		after.fadeIn(300,function(){
+			$(this).attr('style','');
+			if(option.type == 'sub'){
+				$('.sub>.active').removeClass('active');
+				$('.sub,'+option.menu).addClass('active');
+				am.change({obj:$('.sub-default,'+option.menu),reverse:'normal'}).play();
+			} else {
+				after.addClass('active');
+				am.change({obj:after,reverse:'normal'}).play();
+			}
+		})
+	})
+}
+
+//footerBtnClick
+function changePage(){
+	var
+		obj = $('.sub>.page.active'),
+		idx = $('.sub>.page.active').index()-1,
+		min = 0,
+		max = $('.sub>.page').length-1,
+		newObj;
+	if($(this).hasClass('left')){
+		if(--idx < min) idx = max;
+	} else {
+		if(++idx > max) idx = min;
+	}
+	newObj = $('.sub>.page').eq(idx)
+	subView(newObj);
+	$('.gnb .active').removeClass('active')
+	console.log(idx);
+	if(idx < 5){
+		$('.gnb li').eq(0).addClass('active');
+	} else if(idx < 7){
+		$('.gnb li').eq(1).addClass('active');
+	} else if(idx < 14){
+		$('.gnb li').eq(2).addClass('active');
+	} else {
+		$('.gnb li').eq(3).addClass('active');
+	}
+}
+
+function layerOpen(){
+	var key1 = $(this).data("key");
+	var key2 = $(".key",this).html();
+	var obj = layerData[key1][key2];
+	$(".layer-title").html(obj.subject);
+	$(".layer-content").html(obj.content);
+	$(".layer").addClass("active");
+}
---
+++ js/data.js
@@ -0,0 +1,228 @@
+var layerData = {
+	'certification':{
+		'Hyundai Kia Motors SQ certificate':{
+			subject:'Hyundai Kia Motors SQ certificate',
+			content:'\
+			 	<img src="img/certification1.jpg" alt="certification1" />\
+			'
+		},
+		'Certificate of company affiliated research institute':{
+			subject:'Certificate of company affiliated research institute',
+			content:'\
+			 	<img src="img/certification2.jpg" alt="certification2" />\
+			'
+		},
+		'Venture Business Confirmation':{
+			subject:'Venture Business Confirmation',
+			content:'\
+			 	<img src="img/certification3.jpg" alt="certification3" />\
+			'
+		},
+		'Patent No. 10-1298854':{
+			subject:'Patent No. 10-1298854',
+			content:'\
+			 	<img src="img/certification4.jpg" alt="certification4" />\
+			'
+		},
+		'Patent No. 10-0893668':{
+			subject:'Patent No. 10-0893668',
+			content:'\
+			 	<img src="img/certification5.jpg" alt="certification5" />\
+			'
+		},
+		'Patent No. 10-1696068':{
+			subject:'Patent No. 10-1696068',
+			content:'\
+			 	<img src="img/certification6.jpg" alt="certification6" />\
+			'
+		},
+		'Patent No. 10-1717026':{
+			subject:'Patent No. 10-1717026',
+			content:'\
+			 	<img src="img/certification7.jpg" alt="certification7" />\
+			'
+		}
+	},
+	'process':{
+		'BATTERY PACK':{
+			subject:'BATTERY PACK',
+			content:'\
+				<ul class="parts_list">\
+					<li><img src="img/car_1_1.png" alt="car1_1" /></li>\
+					<li><img src="img/car_1_2.png" alt="car1_2" /></li>\
+					<li><img src="img/car_1_3.png" alt="car1_3" /></li>\
+					<li><img src="img/car_1_4.png" alt="car1_4" /></li>\
+					<li><img src="img/car_1_5.png" alt="car1_5" /></li>\
+					<li><img src="img/car_1_6.png" alt="car1_6" /></li>\
+					<li><img src="img/car_1_7.png" alt="car1_7" /></li>\
+					<li><img src="img/car_1_8.png" alt="car1_8" /></li>\
+					<li><img src="img/car_1_9.png" alt="car1_9" /></li>\
+					<li><img src="img/car_1_10.png" alt="car1_10" /></li>\
+					<li><img src="img/car_1_11.png" alt="car1_11" /></li>\
+				</ul>\
+			'
+		},
+		'HAVC/AIR CLEANER':{
+			subject:'HAVC/AIR CLEANER',
+			content:'\
+				<ul class="parts_list">\
+					<li><img src="img/car_2_1.png" alt="car2_1" /></li>\
+					<li><img src="img/car_2_2.png" alt="car2_2" /></li>\
+					<li><img src="img/car_2_3.png" alt="car2_3" /></li>\
+					<li><img src="img/car_2_4.png" alt="car2_4" /></li>\
+					<li><img src="img/car_2_5.png" alt="car2_5" /></li>\
+					<li><img src="img/car_2_6.png" alt="car2_6" /></li>\
+					<li><img src="img/car_2_7.png" alt="car2_7" /></li>\
+					<li><img src="img/car_2_8.png" alt="car2_8" /></li>\
+					<li><img src="img/car_2_9.png" alt="car2_9" /></li>\
+					<li><img src="img/car_2_10.png" alt="car2_10" /></li>\
+					<li><img src="img/car_2_11.png" alt="car2_11" /></li>\
+					<li><img src="img/car_2_12.png" alt="car2_12" /></li>\
+				</ul>\
+			'
+		},
+		'CHC/OIL FILTER':{
+			subject:'CHC/OIL FILTER',
+			content:'\
+				<ul class="parts_list">\
+					<li><img src="img/car_3_1.png" alt="car3_1" /></li>\
+					<li><img src="img/car_3_2.png" alt="car3_2" /></li>\
+					<li><img src="img/car_3_3.png" alt="car3_3" /></li>\
+					<li><img src="img/car_3_4.png" alt="car3_4" /></li>\
+					<li><img src="img/car_3_5.png" alt="car3_5" /></li>\
+				</ul>\
+			'
+		},
+		'AIM':{
+			subject:'AIM',
+			content:'\
+				<ul class="parts_list">\
+					<li><img src="img/car_4_1.png" alt="car4_1" /></li>\
+					<li><img src="img/car_4_2.png" alt="car4_2" /></li>\
+					<li><img src="img/car_4_3.png" alt="car4_3" /></li>\
+					<li><img src="img/car_4_4.png" alt="car4_4" /></li>\
+					<li><img src="img/car_4_5.png" alt="car4_5" /></li>\
+					<li><img src="img/car_4_6.png" alt="car4_6" /></li>\
+				</ul>\
+			'
+		},
+		'DOOR TRIM':{
+			subject:'DOOR TRIM',
+			content:'\
+				<ul class="parts_list">\
+					<li><img src="img/car_5_1.png" alt="car5_1" /></li>\
+					<li><img src="img/car_5_2.png" alt="car5_2" /></li>\
+					<li><img src="img/car_5_3.png" alt="car5_3" /></li>\
+					<li><img src="img/car_5_4.png" alt="car5_4" /></li>\
+					<li><img src="img/car_5_5.png" alt="car5_5" /></li>\
+					<li><img src="img/car_5_6.png" alt="car5_6" /></li>\
+					<li><img src="img/car_5_7.png" alt="car5_7" /></li>\
+					<li><img src="img/car_5_8.png" alt="car5_8" /></li>\
+					<li><img src="img/car_5_9.png" alt="car5_9" /></li>\
+				</ul>\
+			'
+		},
+		'ETC':{
+			subject:'ETC',
+			content:'\
+				<ul class="parts_list">\
+					<li><img src="img/car_6_1.png" alt="car6_1" /></li>\
+					<li><img src="img/car_6_2.png" alt="car6_2" /></li>\
+					<li><img src="img/car_6_3.png" alt="car6_3" /></li>\
+					<li><img src="img/car_6_4.png" alt="car6_4" /></li>\
+					<li><img src="img/car_6_5.png" alt="car6_5" /></li>\
+					<li><img src="img/car_6_6.png" alt="car6_6" /></li>\
+					<li><img src="img/car_6_7.png" alt="car6_7" /></li>\
+					<li><img src="img/car_6_8.png" alt="car6_8" /></li>\
+					<li><img src="img/car_6_9.png" alt="car6_9" /></li>\
+					<li><img src="img/car_6_10.png" alt="car6_10" /></li>\
+					<li><img src="img/car_6_11.png" alt="car6_11" /></li>\
+				</ul>\
+			'
+		},
+	},
+	'cae':{
+		'1':{
+			subject:'CAE',
+			content:'<img src="img/cae1.png" alt="cae1" />'
+		},
+		'2':{
+			subject:'CAE',
+			content:'<img src="img/cae2.png" alt="cae2" />'
+		},
+		'3':{
+			subject:'CAE',
+			content:'<img src="img/cae3.gif" alt="cae3" />'
+		},
+	},
+	'full3d':{
+		'movie':{
+			subject:'Full 3D Design',
+			content:'<div class="video_wrap"><iframe width="800" height="395" src="https://www.youtube.com/embed/Q84epEPqtp8" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div>'
+		},
+	},
+	'cam':{
+		'movie':{
+			subject:'CAM',
+			content:'<div class="video_wrap"><iframe width="800" height="395" src="https://www.youtube.com/embed/TbdH73jAx28" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div>'
+		},
+	},
+	'injection':{
+		'120TON':{
+			subject:'120TON',
+			content:'<img src="img/injection1.png" alt="injection1" />'
+		},
+		'220TON':{
+			subject:'220TON',
+			content:'<img src="img/injection2.png" alt="injection2" />'
+		},
+		'Verticality 200TON':{
+			subject:'Verticality 200TON',
+			content:'<img src="img/injection3.png" alt="injection3" />'
+		},
+		'480TON':{
+			subject:'480TON',
+			content:'<img src="img/injection4.png" alt="injection4" />'
+		},
+		'650TON':{
+			subject:'650TON',
+			content:'<img src="img/injection5.png" alt="injection5" />'
+		},
+		'VIDEO':{
+			subject:'VIDEO',
+			content:'<div class="video_wrap"><iframe width="800" height="395" src="https://www.youtube.com/embed/8yIFa-KlW-E" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div>'
+		},
+	},
+	'injection':{
+		'120TON':{
+			subject:'120TON',
+			content:'<img src="img/injection1.png" alt="injection1" />'
+		},
+		'220TON':{
+			subject:'220TON',
+			content:'<img src="img/injection2.png" alt="injection2" />'
+		},
+		'Verticality 200TON':{
+			subject:'Verticality 200TON',
+			content:'<img src="img/injection3.png" alt="injection3" />'
+		},
+		'480TON':{
+			subject:'480TON',
+			content:'<img src="img/injection4.png" alt="injection4" />'
+		},
+		'650TON':{
+			subject:'650TON',
+			content:'<img src="img/injection5.png" alt="injection5" />'
+		},
+		'VIDEO':{
+			subject:'VIDEO',
+			content:'<div class="video_wrap"><iframe width="800" height="395" src="https://www.youtube.com/embed/8yIFa-KlW-E" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div>'
+		},
+	},
+	'measuring':{
+		'video':{
+			subject:'Measuring &amp; Inspection',
+			content:'<div class="video_wrap"><iframe width="800" height="395" src="https://www.youtube.com/embed/8yIFa-KlW-E" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div>'
+		},
+	}
+}
